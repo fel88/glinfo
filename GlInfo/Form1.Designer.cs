aa9cfceda90397c7c9944812b57118fd218fe2b2
fel88 <15663687+fel88@users.noreply.github.com>
2026-06-30 15:39:50 +0300

    initial

---
+++ GlInfo/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace GlInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new System.Windows.Forms.ListView();
+            columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            textBox1 = new System.Windows.Forms.TextBox();
+            richTextBox1 = new System.Windows.Forms.RichTextBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            panel1 = new System.Windows.Forms.Panel();
+            button8 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            panel2 = new System.Windows.Forms.Panel();
+            statusStrip1 = new System.Windows.Forms.StatusStrip();
+            toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] { columnHeader1, columnHeader2 });
+            listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new System.Drawing.Point(4, 83);
+            listView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            listView1.Name = "listView1";
+            listView1.Size = new System.Drawing.Size(339, 296);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Width = 250;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(10, 8);
+            textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(116, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            richTextBox1.Location = new System.Drawing.Point(351, 83);
+            richTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new System.Drawing.Size(340, 296);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(134, 5);
+            button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(88, 27);
+            button1.TabIndex = 3;
+            button1.Text = "to clipboard";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(228, 4);
+            button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(172, 27);
+            button2.TabIndex = 4;
+            button2.Text = "compare with clipboard";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(407, 5);
+            button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(110, 27);
+            button3.TabIndex = 5;
+            button3.Text = "diff to clipboard";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(listView1, 0, 2);
+            tableLayoutPanel1.Controls.Add(richTextBox1, 1, 2);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 1);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(695, 382);
+            tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            tableLayoutPanel1.SetColumnSpan(panel1, 2);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Location = new System.Drawing.Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(689, 34);
+            panel1.TabIndex = 7;
+            // 
+            // button8
+            // 
+            button8.Location = new System.Drawing.Point(505, 8);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(111, 23);
+            button8.TabIndex = 10;
+            button8.Text = "Create custom";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new System.Drawing.Point(357, 8);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(142, 23);
+            button7.TabIndex = 9;
+            button7.Text = "Create default + comp";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new System.Drawing.Point(240, 6);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(111, 23);
+            button6.TabIndex = 8;
+            button6.Text = "Create default";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(123, 6);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(111, 23);
+            button5.TabIndex = 7;
+            button5.Text = "Create 4.6+comp";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(6, 6);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(111, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Create 4.6";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // panel2
+            // 
+            tableLayoutPanel1.SetColumnSpan(panel2, 2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(button3);
+            panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel2.Location = new System.Drawing.Point(3, 43);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(689, 34);
+            panel2.TabIndex = 8;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new System.Drawing.Point(0, 382);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new System.Drawing.Size(695, 22);
+            statusStrip1.TabIndex = 7;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new System.Drawing.Size(16, 17);
+            toolStripStatusLabel1.Text = "...";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(695, 404);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(statusStrip1);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "GL Info";
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ GlInfo/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.GLControl;
+using OpenTK.Graphics.OpenGL;
+
+namespace GlInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateList()
+        {
+            listView1.Items.Clear();
+            List<string> ext = new List<string>();
+            ext.AddRange(GL.GetString(StringName.Extensions).Split(new char[] { ' ', ';', '\n' }).ToArray());
+            richTextBox1.Text = "";
+            richTextBox1.Text += GL.GetString(StringName.ShadingLanguageVersion);
+
+            foreach (var s in ext)
+            {
+                if (!s.ToLower().Contains(textBox1.Text.ToLower()))
+                    continue;
+
+                listView1.Items.Add(new ListViewItem(new string[] { s }));
+            }
+
+            toolStripStatusLabel1.Text = $"ext count: {ext.Count}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var v in listView1.Items)
+            {
+                var lvi = v as ListViewItem;
+                sb.AppendLine(lvi.Text);
+            }
+            Clipboard.SetText(sb.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var txt = Clipboard.GetText();
+            var arr = txt.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var s in arr)
+            {
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    var txt0 = listView1.Items[i].Text;
+                    if (txt0.ToLower() == s.ToLower())
+                    {
+                        listView1.Items[i].BackColor = Color.Violet;
+
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var v in listView1.Items)
+            {
+                var lvi = v as ListViewItem;
+                if (lvi.BackColor != Color.Violet) continue;
+                sb.AppendLine(lvi.Text);
+            }
+            Clipboard.SetText(sb.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var gl = new GLControl(new GLControlSettings()
+            {
+
+            });
+            gl.SwapBuffers();
+
+            UpdateList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var gl = new GLControl(new GLControlSettings()
+            {
+                APIVersion = new Version(4, 6),
+                Flags = OpenTK.Windowing.Common.ContextFlags.Debug,
+                Profile = OpenTK.Windowing.Common.ContextProfile.Compatability
+            });
+            gl.SwapBuffers();
+
+            UpdateList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var gl = new GLControl(new GLControlSettings()
+            {
+                APIVersion = new Version(4, 6),
+                Flags = OpenTK.Windowing.Common.ContextFlags.Debug,
+
+            });
+            gl.SwapBuffers();
+
+            UpdateList();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var gl = new GLControl(new GLControlSettings()
+            {
+                Profile = OpenTK.Windowing.Common.ContextProfile.Compatability
+            });
+            gl.SwapBuffers();
+
+            UpdateList();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var d = AutoDialog.DialogHelpers.StartDialog();
+            d.AddInt("major", "Major", 4);
+            d.AddInt("minor", "Minor", 6);
+            d.AddBoolField("overrideApiVersion", "Override API version", false);
+            d.AddBoolField("comp", "Compatability profile", false);
+            d.AddBoolField("forw", "Forward compatabible");
+            d.AddBoolField("debug", "Debug");
+
+            d.SetWidth(400);
+
+            if (!d.ShowDialog())
+                return;
+
+            var settings = new GLControlSettings()
+            {
+
+            };
+
+            if (d.GetBoolField("overrideApiVersion"))
+                settings.APIVersion = new Version(d.GetInt("major"), d.GetInt("minor"));
+
+            if (d.GetBoolField("comp"))
+                settings.Profile = OpenTK.Windowing.Common.ContextProfile.Compatability;
+
+            if (d.GetBoolField("forw"))
+                settings.Flags = OpenTK.Windowing.Common.ContextFlags.ForwardCompatible;
+
+            if (d.GetBoolField("debug"))
+                settings.Flags |= OpenTK.Windowing.Common.ContextFlags.Default;
+
+            var gl = new GLControl(settings);
+            gl.SwapBuffers();
+
+            UpdateList();
+        }
+    }
+}
